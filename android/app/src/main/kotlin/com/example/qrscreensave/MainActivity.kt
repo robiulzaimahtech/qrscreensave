7275056ad386227ed5a9bfe68a2215226353a9b9
unknown <robiul.zaimahtech@gmail.com>
2023-12-21 16:20:54 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/qrscreensave/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.qrscreensave
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:share/share.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final GlobalKey globalKey = GlobalKey();
+  final String qrData = "https://www.example.com";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Code Generator"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RepaintBoundary(
+              key: globalKey,
+              child: QrImageView(
+                data: qrData,
+                version: QrVersions.auto,
+                size: 200.0,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => shareQRCode(context),
+              child: Text("Share QR Code"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> shareQRCode(BuildContext context) async {
+    RenderRepaintBoundary boundary = globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List pngBytes = byteData!.buffer.asUint8List();
+
+    // Save the QR code image to a temporary file
+    final tempDir = await getTemporaryDirectory();
+    final file = File('${tempDir.path}/qr_code.png');
+    await file.writeAsBytes(pngBytes);
+
+    // Share the QR code image using the share package
+    Share.shareFiles([file.path], text: "Check out my QR code!");
+  }
+}
